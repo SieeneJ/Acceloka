8b42876e08f2fcfb9d02ae6d31c8ca7f5d0fc302
SieeneJ <Jessika9905@gmail.com>
2026-03-01 02:43:22 +0700

    chore: setup project dependencies and global styles

--- FrontEnd/src/app/layout.tsx
+++ FrontEnd/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
-import Hero from "../components/hero/hero";
-import Navbar from "../components/layouts/navbar";
+import Hero from "@/components/hero/hero";
+import Navbar from "@/components/layouts/navbar";
 import { Schibsted_Grotesk } from "next/font/google";
 
 const schibstedGrotesk = Schibsted_Grotesk({
@@ -17,15 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${schibstedGrotesk.variable}`}>
       <body className={`min-h-screen flex flex-col scroll-smooth`}>
-        <Hero />
         <header className="absolute top-0 left-0 w-full z-50">
           <Navbar />
         </header>
 
-        <main className="flex grow">
-          {children}
-        </main>
-
+        <main className="flex grow">{children}</main>
       </body>
     </html>
   );
